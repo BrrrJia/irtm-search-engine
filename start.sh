2c1fddb6ae434846942e141f50f02a0557dfc169
Jia Jia <93013102+BrrrJia@users.noreply.github.com>
2025-06-13 15:13:56 +0200

    update PORT

--- start.sh
+++ start.sh
@@ -3,12 +3,14 @@
 # Ensure script to exit on error
 set -e
 
+PORT=${PORT:-8501}  # Default to 8501 if PORT is not set
+
 echo "Starting FastAPI..."
 uvicorn src.api.main:app --host 0.0.0.0 --port 8000 &
 
 echo "Starting Streamlit..."
 streamlit run src/streamlit_app.py \
-    --server.port=8501 \
+    --server.port=$PORT \
     --server.address=0.0.0.0 \
     --server.enableCORS=false \
     --server.enableXsrfProtection=false
