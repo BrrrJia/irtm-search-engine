11732fc6a695df96a280284a5464c2886991ccc1
Jia Jia <93013102+BrrrJia@users.noreply.github.com>
2025-06-19 15:16:08 +0200

    fix import error

--- backend/api/main.py
+++ backend/api/main.py
@@ -129,4 +129,4 @@ if __name__ == "__main__":
     # Automatically detect Render's $PORT environment variable, otherwise use the default of 8000.
     port = int(os.environ.get("PORT", 8000))
 
-    uvicorn.run("backend.api.main:app", host="0.0.0.0", port=port)
+    uvicorn.run("api.main:app", host="0.0.0.0", port=port)
