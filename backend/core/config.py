cd9afd57b24e7c180a3b0c7b0145de6808a23b08
Jia Jia <93013102+BrrrJia@users.noreply.github.com>
2025-06-19 16:46:02 +0200

    fix import error

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,14 +4,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 INDEX_DATA_SIZE = int(os.getenv("INDEX_DATA_SIZE", 7000))
-DATA_PATH = os.getenv("DATA_PATH", "data/tweets.csv")
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # core/
+DATA_PATH = os.path.join(BASE_DIR, "..", "data", "tweets.csv")
 
 USE_BIGRAM = os.getenv("USE_BIGRAM", "True") == "True"
 USE_PERMUTERM = os.getenv("USE_PERMUTERM", "False") == "True"
 TOP_K = int(os.getenv("TOP_K", 10))
 
-TRAIN_PATH = os.getenv("TRAIN_PATH", "data/games-train.csv")
-TEST_PATH = os.getenv("TEST_PATH", "data/games-test.csv")
+TRAIN_PATH = os.path.join(BASE_DIR, "..", "data", "games-train.csv")
+TEST_PATH = os.path.join(BASE_DIR, "..", "data", "games-test.csv")
 
 K_MEANS_DATA_SIZE = int(os.getenv("K_MEANS_DATA_SIZE", 200))
 USE_OPTIMAL_KMEANS = os.getenv("USE_OPTIMAL_KMEANS", "False") == "True"
